ee1fd212981af87ed652a941a61f1a4975bc6c4f
Bikee <bikee.bihari@sstsinc.com>
2018-06-21 14:04:08 +0530

    Add a blank line

--- app/src/main/java/com/ba/mobile/bookActivity.java
+++ app/src/main/java/com/ba/mobile/bookActivity.java
@@ -14,8 +14,8 @@ public class bookActivity extends AppCompatActivity {
     Button nonECLoginButton;
 
     Button flightButton;
+//this is test comments
 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
