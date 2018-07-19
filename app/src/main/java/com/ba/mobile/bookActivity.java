40a0f26b4cf626eb28c2c8a47960dcb2622a8cb1
Bikee <bikee.bihari@sstsinc.com>
2018-07-19 20:49:46 +0530

    Added a commented line...

--- app/src/main/java/com/ba/mobile/bookActivity.java
+++ app/src/main/java/com/ba/mobile/bookActivity.java
@@ -23,8 +23,7 @@ public class bookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
-
-
+        
         ecLoginButton = (Button)findViewById(R.id.ecLoginButton);
         nonECLoginButton = (Button)findViewById(R.id.nonECLoginButton);
         flightButton = (Button)findViewById(R.id.flightButton);
